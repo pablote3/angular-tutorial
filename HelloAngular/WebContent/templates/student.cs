786ce15d02db235c9f9cb4cfcfcdcd8cbec45b77
Paul Rossotti <paulro2@vsp.com>
2016-02-29 07:21:17 -0800

    added Angular routing

---
+++ HelloAngular/WebContent/templates/student.cs
@@ -0,0 +1,10 @@
+namespace Demo
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string gender { get; set; }
+        public string city { get; set; }
+    }
+}
