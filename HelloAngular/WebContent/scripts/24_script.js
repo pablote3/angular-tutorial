f13b722d38f38ab06c50161c64320b64a08bf2bf
Paul Rossotti <paulro2@vsp.com>
2016-03-02 08:21:13 -0800

    added StudentService

---
+++ HelloAngular/WebContent/scripts/24_script.js
@@ -0,0 +1,31 @@
+/// <reference path="angular.min.js" />
+var app = angular
+	.module("Demo", ["ngRoute"])
+	.config(function($routeProvider, $locationProvider) {
+		$routeProvider
+			.when("/home", {
+				templateUrl:"../templates/home.html",
+				controller:"homeController"
+			})
+			.when("/courses", {
+				templateUrl:"../templates/courses.html",
+				controller:"coursesController"
+			})
+			.when("/students", {
+				templateUrl:"../templates/students.html",
+				controller:"studentsController"
+			})
+			$locationProvider.html5Mode(true);
+	})
+	.controller("homeController", function($scope) {
+		$scope.message = "Home Page";		
+	})
+	.controller("coursesController", function($scope) {
+		$scope.courses = ["C#", "VB.NET", "SQL Server"];		
+	})
+	.controller("studentsController", function($scope, $http) {
+		$http.get("StudentService.asmx/GetAllStudents")
+			.then(function(response) {
+				$scope.students = response.data;
+			})
+	});
