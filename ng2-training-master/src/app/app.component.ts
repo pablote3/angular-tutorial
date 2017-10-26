a636dfe8b4fe92242de8fb9e7f34332aea1985e1
Paul Rossotti <rossotti.paul@gmail.com>
2017-10-26 14:42:14 -0700

    form validation

--- ng2-training-master/src/app/app.component.ts
+++ ng2-training-master/src/app/app.component.ts
@@ -3,23 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-      <app-hello></app-hello>
-      <app-my-first-component></app-my-first-component>
-      <app-model></app-model>
-      
-      Person Service
-      <app-ngfor></app-ngfor>
-  
-      asynch
-      <app-ngfor-asynch></app-ngfor-asynch>
-      
-      <!-- app-hiding></app-hiding -->
-    
-      <app-ngswitch></app-ngswitch>
-      <app-ng-class></app-ng-class>
-    
-      <app-router></app-router>
-    
+      <!--<app-hello></app-hello>-->
+      <!--<app-my-first-component></app-my-first-component>-->
+      <!--<app-model></app-model>-->
+      <!---->
+      <!--Person Service-->
+      <!--<app-ngfor></app-ngfor>-->
+  <!---->
+      <!--asynch-->
+      <!--<app-ngfor-asynch></app-ngfor-asynch>-->
+      <!---->
+      <!--&lt;!&ndash; app-hiding></app-hiding &ndash;&gt;-->
+    <!---->
+      <!--<app-ngswitch></app-ngswitch>-->
+      <!--<app-ng-class></app-ng-class>-->
+    <!---->
+      <!--<app-router></app-router>-->
+      <app-forms></app-forms>
   `
 })
 
