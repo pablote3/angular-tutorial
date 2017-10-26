f8f0f7fa21aa4c2ea45e3c36869a42e0de9f97c9
Paul Rossotti <rossotti.paul@gmail.com>
2017-10-26 13:15:18 -0700

    morning session

--- ng2-training-master/src/app/app.routing.ts
+++ ng2-training-master/src/app/app.routing.ts
@@ -4,16 +4,24 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgifComponent } from './ngif/ngif.component';
 import {HelloComponent} from "./hello/hello.component";
 import {HttpComponent} from "./http/http.component";
+import {LoginComponent} from "./router/login/login.component";
+import {AuthGuard} from "./router/auth-guard-service";
 
 const appRoutes: Routes = [
   {
-    path: 'if', component: NgifComponent
+    path: 'if', component: NgifComponent, canActivate: [AuthGuard]
   },
   {
-    path: 'hello/:name', component: HelloComponent
+    path: 'hello/:name', component: HelloComponent, canActivate: [AuthGuard]
   },
   {
-    path: 'http', component: HttpComponent
+    path: 'http', component: HttpComponent, canActivate: [AuthGuard]
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: '', component: LoginComponent
   }
 ];
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
--- ng2-training-master/src/app/router/auth-guard-service.ts
+++ ng2-training-master/src/app/router/auth-guard-service.ts
@@ -2,16 +2,17 @@
 import { Injectable }     from '@angular/core';
 import { CanActivate }    from '@angular/router';
 import {Observable} from "rxjs";
+import {LoginService} from "./login/login.service";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(){
+  constructor(private service: LoginService){
   }
 
   canActivate() : Observable<boolean> {
     console.log('AuthGuard#canActivate called');
-    return Observable.of(true);
+    return Observable.of(this.service.isUserLoggedIn());
   }
 
 }
