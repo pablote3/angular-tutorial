06125386619daa1cc0b94ff1f4b4a29a86cc3151
Paul Rossotti <rossotti.paul@gmail.com>
2017-10-27 09:36:45 -0700

    update test case

--- ng2-training-master/src/app/component/component.component.spec.ts
+++ ng2-training-master/src/app/component/component.component.spec.ts
@@ -1,10 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ComponentComponent } from './component.component';
+import {By} from "@angular/platform-browser";
 
 describe('ComponentComponent', () => {
   let component: ComponentComponent;
   let fixture: ComponentFixture<ComponentComponent>;
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -27,7 +29,8 @@ describe('ComponentComponent', () => {
     expect(true).toBe(true);
   });
 
-  it('should check content', () => {
-    expect(true).toBe(true);
+  it('element should contain', () => {
+    let p = fixture.debugElement.query(By.css("p")).nativeElement
+    expect(p.textContent).toContain('component works!');
   });
 })
