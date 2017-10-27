787a2fdece40009e4c5bc5c13a560aa786cce014
Paul Rossotti <rossotti.paul@gmail.com>
2017-10-27 11:07:33 -0700

    initial commit

---
+++ ng-weather/src/app/current-conditions/current-conditions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-current-conditions',
+  templateUrl: './current-conditions.component.html',
+  styleUrls: ['./current-conditions.component.css']
+})
+export class CurrentConditionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
